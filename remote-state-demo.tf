f49525cde9bf0e624cb13b730c11c75c3e3f7962
Nirmal <nrmlshrma07@gmail.com>
2021-06-19 13:21:02 +0530

    data source in place

--- remote-state-demo.tf
+++ remote-state-demo.tf
@@ -7,7 +7,13 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t2.micro"
   name                 = "mydb"
   username             = "foo"
-  password             = file("${path.module}/confidential.txt")
+  #password            = file("${path.module}/confidential.txt")
+  password             = data.aws_ssm_parameter.dummyref.arn
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+}
+
+#data source is use when you retrive from aws resource
+data "aws_ssm_parameter" "dummyref" {
+    name = "MYPASSWD"
 }
